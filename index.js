754093af0f8ed15798cc52ba5a326c96468e3f5f
Phil Balchin <phil@maniacalrobot.co.uk>
2017-03-02 10:41:24 +0000

    does it work?

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express')
 var app = express()
+var redis = require("redis"),
+  redisClient = redis.createClient();
  
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', function (request, response) {
+  response.send('Hello World 2')
+})
+
+app.get('/:key', function(request, response) {
+
+  redisClient.get('foo', function(err, value) {
+    response.send(value)
+  })
 })
  
 app.listen(process.env.PORT || 8080)
